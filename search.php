cdc6fe54a3e348ea7a946d6979dfa31879cadb3f
Serhat Abdulbakioğlu <serhat359@gmail.com>
2016-07-31 17:29:25 +0300

    refactor

--- search.php
+++ search.php
@@ -1,20 +1,17 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-	        "http://www.w3.org/TR/html4/loose.dtd">
-	<html lang="en">
-	<head>
-	    <meta http-equiv="content-type" content="text/html; charset=utf-8">
-	    <title>Youtube Better Search</title>
-	 </head>
-	 <body>
-	 <?php   
-	 echo $_GET['user'] . ' videos';
-	 
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<title>Youtube Better Search</title>
+</head>
+<body>
+	<?php
+	echo $_GET['user'] . ' videos';
 	 ?>
-	<label id="debug"></label>
-	
-	<table id="results"></table>
-	 <script type="text/javascript" src="/jquery/jquery-3.0.0.js"></script>
-	 <script type="text/javascript">
+<label id="debug"></label>
+<table id="results"></table>
+<script type="text/javascript" src="/jquery/jquery-3.0.0.js"></script>
+<script type="text/javascript">
 	 var apikey = 'AIzaSyBrbMk0ZR5638IrTlVBTra7NLYdoBjUt2o';
 
 // my version
@@ -25,67 +22,56 @@ function getChannelID(user, callback){
 	var result;
 	
 	$.get(
-   "https://www.googleapis.com/youtube/v3/channels",{
-	part : 'contentDetails', 
-	forUsername : user,
-	key: apikey
-   },
-   function(data) {
-		
-          pid = data.items[0].id;
-          result = pid;
-		  callback(result);
-      
-  }
-);
+	"https://www.googleapis.com/youtube/v3/channels",
+	{
+		part : 'contentDetails', 
+		forUsername : user,
+		key: apikey
+	},
+	function(data) {
+		pid = data.items[0].id;
+		result = pid;
+		callback(result);
+	});
 }
 
 getChannelID(uname, function(id){
-	
-$.get(
-   "https://www.googleapis.com/youtube/v3/search?key="+apikey+"&channelId="+id+"&part=snippet&maxResults=50&order=rating",
-	{
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search?key="+apikey+"&channelId="+id+"&part=snippet&maxResults=50&order=rating",
+		{
+			
+		},
+		function(data) {
+			
+			// set the statistics
+			
+			setStats(data);
+			
+			data.items = data.items.filter(function(x){ return x.statistics.viewCount > 20000 });
+			
+			// sort the datas
+			
+			data.items.sort((x,y) => {
+				return getValue(y) > getValue(x);
+			});
 		
-	},
-   function(data) {
-	   
-	   function getValue(item){
-		   if(item.statistics.dislikeCount == 0)
-			   item.statistics.dislikeCount = 1;
-		   
-		   var ratio = item.statistics.likeCount / (item.statistics.likeCount + item.statistics.dislikeCount);
-		   return ratio * ratio * item.statistics.viewCount;
-	   }
-	   
-	   // set the statistics
-	   
-	   setStats(data);
-	   
-	   data.items = data.items.filter(function(x){ return x.statistics.viewCount > 20000 });
-	   
-	  // sort the datas
-	  
-	  data.items.sort( (x,y) => {
-		  return getValue(y) > getValue(x);
-	  } );
-	   
-      $.each( data.items, function( i, item ) {
-          
-		  var labeled = '<p><a href="https://www.youtube.com/watch?v='+ item.id.videoId +'">'+item.snippet.title +'</a></p>';
-		  
-		  var views = '<p> View count: '+item.statistics.viewCount + ' ' +'</p>';
-		  
-		  var score = '<p> Score: '+ item.statistics.ratio + ' ' +'</p>';
-		  
-		  var extraScores = '<p> Like: ' + item.statistics.likeCount + ' , dislike: ' + item.statistics.dislikeCount +'</p>';
-		  
-		  var imged = '<img src="'+item.snippet.thumbnails.default.url+'">';
-		  
-		  results = '<tr>'+ columnize(imged) + columnize(labeled + views + score + extraScores) +'</tr>';
-                $('#results').append(results);
-      });
-  }
-);
+			$.each( data.items, function( i, item ) {
+				
+				var labeled = '<p><a href="https://www.youtube.com/watch?v='+ item.id.videoId +'">'+item.snippet.title +'</a></p>';
+				
+				var views = '<p> View count: '+item.statistics.viewCount + ' ' +'</p>';
+				
+				var score = '<p> Rating: '+ item.statistics.ratio + ' ' +'</p>';
+				
+				var extraScores = '<p> Like: ' + item.statistics.likeCount + ' , dislike: ' + item.statistics.dislikeCount +'</p>';
+				
+				var imged = '<img src="'+item.snippet.thumbnails.default.url+'">';
+				
+				results = '<tr>'+ columnize(imged) + columnize(labeled + views + score + extraScores) +'</tr>';
+						$('#results').append(results);
+			});
+		}
+	);
 });
 
 function setStats(data){
@@ -119,6 +105,15 @@ function columnize(data){
 function debug(data){
 	$('label').html(JSON.stringify(data));
 }
-	 </script>
-	</body>
-	</html>
+
+function getValue(item){
+	if(item.statistics.dislikeCount == 0)
+		item.statistics.dislikeCount = 1;
+	
+	var ratio = item.statistics.likeCount / (item.statistics.likeCount + item.statistics.dislikeCount);
+	return ratio * ratio * item.statistics.viewCount;
+}
+	   
+</script>
+</body>
+</html>
